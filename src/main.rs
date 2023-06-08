2e08743ec01719fdb53d02f2160e3d68ed2e6289
Adam Barton <108860307+rosingrind@users.noreply.github.com>
2023-06-09 00:51:09 +0300

    .toml config support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+mod cipher;
+mod stream;
+
+use serde::Deserialize;
+use std::net::Ipv4Addr;
+use std::{fs, path::Path};
+
+use crate::cipher::{CipherSpec, NonceSpec};
+
+#[derive(Debug, Deserialize)]
+struct Data {
+    #[serde(default)]
+    cipher: CipherSpec,
+    #[serde(default)]
+    nonce: NonceSpec,
+    udp_a: UDP,
+    udp_b: UDP,
+}
+
+#[derive(Debug, Deserialize)]
+struct UDP {
+    ip: Ipv4Addr,
+    port: u16,
+}
+
+const DATA_PATH: &str = "./res/data";
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new(DATA_PATH).with_extension("toml");
+    toml::from_str::<Data>(&*fs::read_to_string(path).unwrap()).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn config_valid() {
+        let path = Path::new(DATA_PATH).with_extension("toml");
+        toml::from_str::<Data>(&*fs::read_to_string(path).unwrap()).unwrap();
+    }
 }
