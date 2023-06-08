62db5d462d8b4bf059a1aaa2089c963ec93760e0
Adam Barton <108860307+rosingrind@users.noreply.github.com>
2023-06-09 00:42:59 +0300

    dyn Stream module + UDP socket backend

---
+++ src/stream.rs
@@ -0,0 +1,48 @@
+mod socket;
+
+use std::io;
+use std::net::{SocketAddr, ToSocketAddrs};
+use std::sync::Arc;
+
+use crate::stream::socket::Socket;
+
+pub(super) trait IOStream {
+    fn connect(&self, addr: SocketAddr) -> io::Result<()>;
+
+    fn poll(&self) -> io::Result<Vec<u8>>;
+
+    fn push(&self, buf: &[u8]) -> io::Result<()>;
+}
+
+// TODO: add config details
+pub(super) fn get_stream<A: ToSocketAddrs>(addr: A) -> Arc<dyn IOStream + Sync + Send> {
+    Arc::new(Socket::new(addr).unwrap())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::net::{Ipv4Addr, SocketAddr};
+
+    #[test]
+    fn stream_works() {
+        let ip: Ipv4Addr = Ipv4Addr::from([127, 0, 0, 1]);
+        const PORT_A: u16 = 34254;
+        const PORT_B: u16 = 34250;
+
+        let addr_a: SocketAddr = SocketAddr::new(ip.into(), PORT_A);
+        let addr_b: SocketAddr = SocketAddr::new(ip.into(), PORT_B);
+
+        let stream_a = get_stream(addr_a);
+        let stream_b = get_stream(addr_b);
+
+        stream_a.connect(addr_b).unwrap();
+        stream_b.connect(addr_a).unwrap();
+
+        let buf = "data";
+
+        stream_a.push(buf.as_ref()).unwrap();
+        let res = stream_b.poll().unwrap();
+        assert_eq!(res.as_slice(), buf.as_bytes());
+    }
+}
---
+++ src/stream/socket.rs
@@ -0,0 +1,42 @@
+use std::io;
+use std::net::{SocketAddr, ToSocketAddrs, UdpSocket};
+
+use crate::stream::IOStream;
+
+const MAX_BUF: usize = 250;
+
+pub(super) struct Socket {
+    socket: UdpSocket,
+}
+
+// TODO: implement address struct?
+impl Socket {
+    pub fn new<A: ToSocketAddrs>(addr: A) -> Result<Socket, io::Error> {
+        match UdpSocket::bind(addr) {
+            Ok(socket) => Ok(Socket { socket }),
+            Err(err) => Err(err),
+        }
+    }
+}
+
+impl IOStream for Socket {
+    fn connect(&self, addr: SocketAddr) -> io::Result<()> {
+        self.socket.connect(addr)
+    }
+
+    fn poll(&self) -> io::Result<Vec<u8>> {
+        let mut buf = [0; MAX_BUF];
+
+        match self.socket.recv(buf.as_mut()) {
+            Ok(len) => Ok(buf[..len].into()),
+            Err(err) => Err(err),
+        }
+    }
+
+    fn push(&self, buf: &[u8]) -> io::Result<()> {
+        match self.socket.send(buf) {
+            Ok(_) => Ok(()),
+            Err(err) => Err(err),
+        }
+    }
+}
